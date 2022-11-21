c4fb5344f0be52421a03b824cf808843c4d183f4
root <root@debian.DannyGersh>
2022-11-21 08:48:02 -0500

    auto push

--- extractIp.py
+++ extractIp.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
 ip = None
-with open('/main/myStuff/ip', 'r') as f:
+with open('ip', 'r') as f:
 	ip = f.read()
 f.close()
 
 ip = ip.split(' ')
 ip = ip[1]
-with open('/main/myStuff/ip', 'w') as f:
+with open('ip', 'w') as f:
 	f.write(ip)
+f.close()
+
+with open('ip', 'a') as f:
+	f.write(datetime.now().strftime("%H:%M:%S"))
 f.close()

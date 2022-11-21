7f3391ba89dee498918a000f3d9f7732ef5e4d9c
root <root@debian.DannyGersh>
2022-11-21 16:21:01 -0500

    auto push

--- extractIp.py
+++ extractIp.py
@@ -7,10 +7,11 @@ f.close()
 
 ip = ip.split(' ')
 ip = ip[1]
+
 with open('ip', 'w') as f:
 	f.write(ip)
 f.close()
 
 with open('track', 'a') as f:
-	f.write(str(datetime.now())+'\n')
+	f.write(str(datetime.now())+' : '+str(ip)+'\n')
 f.close()

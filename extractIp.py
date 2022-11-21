6ac542dfb1e118b2cbfe5326483d0d77d4dd463f
root <root@debian.DannyGersh>
2022-11-21 08:41:01 -0500

    auto push

--- extractIp.py
+++ extractIp.py
@@ -1,10 +1,10 @@
 ip = None
-with open('ip', 'r') as f:
+with open('/main/myStuff/ip', 'r') as f:
 	ip = f.read()
 f.close()
 
 ip = ip.split(' ')
 ip = ip[1]
-with open('ip', 'w') as f:
+with open('/main/myStuff/ip', 'w') as f:
 	f.write(ip)
 f.close()
